fa98f287b9ce75cdf8e75899a5b63f0cd4c0fd77
Metin1 <metin3232@yahoo.com>
2022-05-10 17:50:17 +0300

    fix: remove unused stuffs

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import * as THREE from 'three'
-import { useMemo, useRef } from 'react'
-import { Canvas, applyProps, useFrame } from '@react-three/fiber'
-import { Environment, Lightformer, Float, useGLTF, BakeShadows, ContactShadows, OrbitControls } from '@react-three/drei'
-import { LayerMaterial, Base, Depth } from 'lamina'
+import { useMemo,  } from 'react'
+import { Canvas, applyProps } from '@react-three/fiber'
+import { useGLTF, OrbitControls } from '@react-three/drei'
+
 
 export const App = () => (
   <Canvas shadows dpr={[1, 2]} camera={{ position: [-10, 0, 15], fov: 30 }}>
     <OrbitControls />
-
     <Porsche scale={0.6} position={[-2, -0.78, 0]} />
     <Porsche scale={0.6} position={[2, -0.78, 0]} />
-    <spotLight position={[0, 15, 0]} angle={0.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
-    <ambientLight intensity={0.2} />
-    <ContactShadows resolution={1024} frames={1} position={[0, -1.16, 0]} scale={10} blur={3} opacity={1} far={10} />
-
-    {/* Renders contents "live" into a HDRI environment (scene.environment). */}
-    <Environment frames={Infinity} resolution={256}>
-      {/* Ceiling */}
-      <Lightformer intensity={0.75} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
-      {/* Sides */}
-      <Lightformer intensity={4} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[20, 0.1, 1]} />
-      <Lightformer rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[20, 0.5, 1]} />
-      <Lightformer rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[20, 1, 1]} />
-      {/* Background */}
-      <mesh scale={100}>
-        <sphereGeometry args={[1, 64, 64]} />
-        <LayerMaterial side={THREE.BackSide}>
-          <Base color="#444" alpha={1} mode="normal" />
-          <Depth colorA="blue" colorB="black" alpha={0.5} mode="normal" near={0} far={300} origin={[100, 100, 100]} />
-        </LayerMaterial>
-      </mesh>
-    </Environment>
-
-    <BakeShadows />
+    <spotLight position={[0, 15, 0]} angle={0.3} penumbra={1} castShadow intensity={7} shadow-bias={-0.0001} />
   </Canvas>
 )
 
@@ -47,26 +24,4 @@ function Porsche(props) {
   }, [nodes, materials])
   const copiedScene = useMemo(() => scene.clone(), [scene])
   return <primitive object={copiedScene} {...props} />
-}
-
-function CameraRig({ v = new THREE.Vector3() }) {
-  return useFrame((state) => {
-    const t = state.clock.elapsedTime
-    state.camera.position.lerp(v.set(Math.sin(t / 5), 0, 10 + Math.cos(t / 5)), 0.05)
-    state.camera.lookAt(0, 0, 0)
-  })
-}
-
-function MovingSpots({ positions = [2, 0, 2, 0, 2, 0, 2, 0] }) {
-  const group = useRef()
-  useFrame((state, delta) => (group.current.position.z += delta * 15) > 60 && (group.current.position.z = -60))
-  return (
-    <group rotation={[0, 0.5, 0]}>
-      <group ref={group}>
-        {positions.map((x, i) => (
-          <Lightformer form="circle" intensity={4} rotation={[Math.PI / 2, 0, 0]} position={[x, 4, i * 4]} scale={[3, 1, 1]} />
-        ))}
-      </group>
-    </group>
-  )
 }
--- src/index.js
+++ src/index.js
@@ -5,28 +5,12 @@ Sketchfab: https://sketchfab.com/3d-models/free-porsche-911-carrera-4s-d01b25448
 
 import { render } from 'react-dom'
 import { Suspense } from 'react'
-import { Footer } from '@pmndrs/branding'
 import './styles.css'
 import { App } from './App'
 
 render(
   <Suspense fallback={null}>
     <App />
-    <Overlay />
   </Suspense>,
   document.getElementById('root')
 )
-
-function Overlay() {
-  return (
-    <>
-      <Footer
-        date="06. March"
-        year="2022"
-        link1={<a href="https://github.com/pmndrs/drei">pmndrs/drei</a>}
-        link2={<a href="https://codesandbox.io/s/building-live-envmaps-lwo219">s/lwo219</a>}
-      />
-      <div style={{ color: '#808080', position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>neutral —</div>
-    </>
-  )
-}
